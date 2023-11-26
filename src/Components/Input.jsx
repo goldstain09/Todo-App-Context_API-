600331df921a38b20e280e96f7e11ced128900f2
Sujal RAjput <rajputsujal992@gmail.c0m>
2023-11-26 16:19:02 +0530

    code formatting

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,65 +1,71 @@
-import React, { useContext, useEffect, useState } from 'react';
-import './TodoHome.css';
-import Store from '../Context/Todo.Store';
+import React, { useContext, useEffect, useState } from "react";
+import "./TodoHome.css";
+import Store from "../Context/Todo.Store";
 
 export default function Input() {
+  // using context for getting data from store
+  const cntxt = useContext(Store);
 
-    // using context for getting data from store
-    const cntxt = useContext(Store);
+  // using useState for setting the value of input in inputData..
+  const [inputData, setInputData] = useState("");
 
-    // using useState for setting the value of input in inputData..
-    const [inputData, setInputData] = useState('');
+  // it's for popuping an error of empty value on adding empty value
+  const [emptyError, setEmptyError] = useState(false);
 
-    // it's for popuping an error of empty value on adding empty value
-    const [emptyError, setEmptyError] = useState(false);
-
-    const addInputData = () => {
-        if (inputData.length > 0) {
-            if (cntxt.Edit_is) {
-                cntxt.updateTodo(inputData, cntxt.DataForEdit.unique_ID);
-            } else {
-                cntxt.addTodo(inputData);
-            }
-        } else {
-            setEmptyError(true)
-        }
-
-        setInputData('')
+  const addInputData = () => {
+    if (inputData.length > 0) {
+      if (cntxt.Edit_is) {
+        cntxt.updateTodo(inputData, cntxt.DataForEdit.unique_ID);
+      } else {
+        cntxt.addTodo(inputData);
+      }
+    } else {
+      setEmptyError(true);
     }
 
+    setInputData("");
+  };
 
-    useEffect(() => {
-        setInputData(cntxt.DataForEdit.item)
-    }, [cntxt.Edit_is, cntxt.DataForEdit.item, cntxt.DataForEdit.unique_ID])
+  useEffect(() => {
+    setInputData(cntxt.DataForEdit.item);
+  }, [cntxt.Edit_is, cntxt.DataForEdit.item, cntxt.DataForEdit.unique_ID]);
 
-    return (
-        <div>
-            <div className={emptyError ? 'Input_Div_forError d-flex align-content-center' : 'Input_Div d-flex align-content-center'}>
-
-                <input
-                    placeholder='Type here...'
-                    type='text'
-                    className='Todo_input'
-                    autoFocus
-                    value={inputData}
-                    onChange={(event) => {
-                        setInputData(event.target.value);
-                    }}
-                    // both are same I used two times bcz of learning purpose...
-                    onInput={() => {
-                        setEmptyError(false);
-                    }} />
-                <button onClick={addInputData} className='Add_btn'><i className={cntxt.Edit_is ? 'bi bi-journal-arrow-up' : 'bi bi-plus-square'}></i></button>
-
-            </div>
-            {
-                emptyError && (
-                    <div className='error_Div'>
-                        <p className='text-danger'>Please type something before adding...</p>
-                    </div>
-                )
+  return (
+    <div>
+      <div
+        className={
+          emptyError
+            ? "Input_Div_forError d-flex align-content-center"
+            : "Input_Div d-flex align-content-center"
+        }
+      >
+        <input
+          placeholder="Type here..."
+          type="text"
+          className="Todo_input"
+          autoFocus
+          value={inputData}
+          onChange={(event) => {
+            setInputData(event.target.value);
+          }}
+          // both are same I used two times bcz of learning purpose...
+          onInput={() => {
+            setEmptyError(false);
+          }}
+        />
+        <button onClick={addInputData} className="Add_btn">
+          <i
+            className={
+              cntxt.Edit_is ? "bi bi-journal-arrow-up" : "bi bi-plus-square"
             }
-
+          ></i>
+        </button>
+      </div>
+      {emptyError && (
+        <div className="error_Div">
+          <p className="text-danger">Please type something before adding...</p>
         </div>
-    )
+      )}
+    </div>
+  );
 }
--- src/Components/TodoHome.jsx
+++ src/Components/TodoHome.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import Input from './Input';
-import Lists from './Lists';
-import './TodoHome.css';
+import React from "react";
+import Input from "./Input";
+import Lists from "./Lists";
+import "./TodoHome.css";
 
 export default function TodoHome() {
   return (
-    <div className='container-fluid'>
-        <div className='container'>
-            <div className='row'>
-            <Input  />
-            <Lists  />
-            </div>
+    <div className="container-fluid">
+      <div className="container">
+        <div className="row">
+          <Input />
+          <Lists />
         </div>
+      </div>
     </div>
-  )
+  );
 }
